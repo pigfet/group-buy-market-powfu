00dca3cd400e93bfdb30c481d422f0daaa86b7fc
pigfet <127737289@qq.com>
2026-03-28 21:10:07 +0800

    2.14 拼团回调通知任务 测试未通过

--- group-buy-market-powfu-domain/src/main/java/com/chd/domain/trade/adapter/repository/ITradeRepository.java
+++ group-buy-market-powfu-domain/src/main/java/com/chd/domain/trade/adapter/repository/ITradeRepository.java
@@ -5,7 +5,10 @@ import com.chd.domain.trade.model.aggregate.GroupBuyTeamSettlementAggregate;
 import com.chd.domain.trade.model.entity.GroupBuyActivityEntity;
 import com.chd.domain.trade.model.entity.GroupBuyTeamEntity;
 import com.chd.domain.trade.model.entity.MarketPayOrderEntity;
+import com.chd.domain.trade.model.entity.NotifyTaskEntity;
 import com.chd.domain.trade.model.valobj.GroupBuyProgressVO;
+
+import java.util.List;
 
 /**
  * @author Fuzhengwei bugstack.cn @小傅哥
@@ -26,8 +29,18 @@ public interface ITradeRepository {
 
     GroupBuyTeamEntity queryGroupBuyTeamByTeamId(String teamId);
 
-    void settlementMarketPayOrder(GroupBuyTeamSettlementAggregate groupBuyTeamSettlementAggregate);
+    boolean settlementMarketPayOrder(GroupBuyTeamSettlementAggregate groupBuyTeamSettlementAggregate);
 
     boolean isSCBlackIntercept(String source, String channel);
+
+    List<NotifyTaskEntity> queryUnExecutedNotifyTaskList();
+
+    List<NotifyTaskEntity> queryUnExecutedNotifyTaskList(String teamId);
+
+    int updateNotifyTaskStatusSuccess(String teamId);
+
+    int updateNotifyTaskStatusError(String teamId);
+
+    int updateNotifyTaskStatusRetry(String teamId);
 
 }
--- group-buy-market-powfu-infrastructure/src/main/java/com/chd/infrastructure/dao/INotifyTaskDao.java
+++ group-buy-market-powfu-infrastructure/src/main/java/com/chd/infrastructure/dao/INotifyTaskDao.java
@@ -3,6 +3,8 @@ package com.chd.infrastructure.dao;
 import com.chd.infrastructure.dao.po.NotifyTask;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 /**
  * @author Fuzhengwei bugstack.cn @小傅哥
  * @description 回调任务
@@ -12,5 +14,15 @@ import org.apache.ibatis.annotations.Mapper;
 public interface INotifyTaskDao {
 
     void insert(NotifyTask notifyTask);
+
+    List<NotifyTask> queryUnExecutedNotifyTaskList();
+
+    NotifyTask queryUnExecutedNotifyTaskByTeamId(String teamId);
+
+    int updateNotifyTaskStatusSuccess(String teamId);
+
+    int updateNotifyTaskStatusError(String teamId);
+
+    int updateNotifyTaskStatusRetry(String teamId);
 
 }
